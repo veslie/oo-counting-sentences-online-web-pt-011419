41c1ee0d8e971fab5d741b557f4400e9838ba166
veslie losius <veslielosius@yahoo.com>
2019-02-16 21:56:58 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
+    "Hi, my name is Sophie.".sentence? = true 
+  end 
+  "Hi, my name is Sophie".sentence? = false 
     
-  end
+     end
 
   def question?
+    "what's your name?".questions? = true 
+  end 
+  "what's your name".question? = false 
 
   end
 
   def exclamation?
-
+"Hi, my name is Sophie!".exclamation = true 
+end 
+"Hi, my name is Sophie".exclamation = true 
   end
 
   def count_sentences
+    ("one", "two", "three").count_sentences = (3)
 
   end
 end
