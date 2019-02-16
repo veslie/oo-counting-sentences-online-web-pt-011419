e77f9c869500456b07095b4e5ff876eb7664b210
veslie losius <veslielosius@yahoo.com>
2019-02-16 21:52:34 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,12 +3,16 @@ require 'pry'
 class String
 
   def sentence?
-    "Hi, my name is Sophie".sentence = true 
+    "Hi, my name is Sophie.".sentence = true 
+  end 
+  "Hi, my name is Sophie".sentence = false 
     
      end
 
   def question?
     "what's your name?".questions? = true 
+  end 
+  "what's your name".question? = false 
 
   end
 
